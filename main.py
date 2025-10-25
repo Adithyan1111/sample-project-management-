259410a53f5991a0edf5909bd32e74d97b68ac12
Adithyan1111 <gadithyan2005@gmail.com>
2025-10-25 13:03:57 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# sample-project-management
+## Description
+A simple python program to calculate student grades based on marks input by the user
+## team member
+Adithyan G
+## features
+-accepts marks for multiple subjects
+-calculate average and grade
+-displays result summary
+## how to run 
+clone the responsitary:'git clone https://github.com/Adithyan1111/main.py navigate to the folder:'cd main.py' run the program:'python main.py'
+## tools/libraries used
+languages :python project management:github issues and kanban board
+## contributers
+Adithyan G
